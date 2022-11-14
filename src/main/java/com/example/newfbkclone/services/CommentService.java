76dbc0d6b6e73ae855856b4e21f7cae20ae07f2a
bob160 <efioesien@gmail.com>
2022-11-14 01:51:27 +0100

    finished comment service implementation

--- src/main/java/com/example/newfbkclone/services/CommentService.java
+++ src/main/java/com/example/newfbkclone/services/CommentService.java
@@ -1,11 +1,16 @@
 package com.example.newfbkclone.services;
 
 
+import com.example.newfbkclone.entities.Comment;
 import com.example.newfbkclone.pojos.CommentDto;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface CommentService {
+    void addComment(Comment comment);
 
+    void updateComment(Comment editedComment);
+
+    void deleteComment(Long commentId);
 
 }
--- src/main/java/com/example/newfbkclone/services/serviceImpl/CommentServiceImpl.java
+++ src/main/java/com/example/newfbkclone/services/serviceImpl/CommentServiceImpl.java
@@ -1,8 +1,26 @@
 package com.example.newfbkclone.services.serviceImpl;
 
+import com.example.newfbkclone.entities.Comment;
+import com.example.newfbkclone.repositories.CommentRepository;
 import com.example.newfbkclone.services.CommentService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CommentServiceImpl implements CommentService {
+
+    CommentRepository commentRepository;
+    @Override
+    public void addComment(Comment comment) {
+        commentRepository.save(comment);
+    }
+
+    @Override
+    public void updateComment(Comment editedComment) {
+        commentRepository.save(editedComment);
+    }
+
+    @Override
+    public void deleteComment(Long commentId) {
+        commentRepository.deleteCommentByCommentId(commentId);
+    }
 }
